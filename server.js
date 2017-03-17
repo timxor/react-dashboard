391e2ea25408870abc896e003927cc8a4f899b27
Tim Siwula <tcsiwula@gmail.com>
2017-03-16 19:21:20 -0700

    :hamster: Chapter 1 : Installation and Setup

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-// to start app run the following in separate terminals:
-// 1: geth --rpcapi eth,web3,personal --rpc
-// 2: node server
+const express = require('express');
+const path = require('path');
 
-var express = require('./config/express');
-
-/* Create a new Web3 connections instance; did not work 12/30/16 BR*/
-//var web3 = web3();
-
-/* Create a new Express application instance */
 var app = express();
 
-/* Use the Express application instance to listen to the '3000' port */
-app.listen(3000);
+
 
 /* Log the server status to the console */
 console.log('Happy block live on port http://localhost:3000!')
